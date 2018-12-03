6a2a8ec29908d7de46f45e4a1db07745ac8e8ba6
Aaron Yeh <44712755+aayeh@users.noreply.github.com>
2018-12-03 10:34:04 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -8,4 +8,20 @@ do
    case $opt in
       "Option1")
          echo "Please input the year: "
-         
+         read year
+         echo "Please input the make: "
+         read make
+         echo "Please input the model: "
+         read model
+         echo "$year:$make:$model" >> My_old_cars
+      ;;
+    "Option 2")
+      sort My_old_cars
+      read -r My_old_cars
+      ;;
+    "Quit")
+         echo "Goodbye!"
+         break
+      ;;
+   esac
+done
