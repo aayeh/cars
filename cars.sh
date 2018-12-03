e0e2c8b8adbf768ba750276bdce41ed11651391b
Aaron Yeh <44712755+aayeh@users.noreply.github.com>
2018-12-03 10:21:45 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -2,6 +2,4 @@
 # cars.sh
 # Aaron Yeh
 while 
-   echo "Choose (1) to enter a car 
-         Choose (2) to list the cars 
-	 Choose (q) to quit"
+   echo "Choose (1) to enter a car Choose (2) to list the cars Choose (q) to quit"
